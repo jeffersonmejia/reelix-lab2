1c64daad8f78705ebed1ddcf254b3f419d0374f9
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2025-05-28 21:43:22 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const d = document,
+  $video = d.querySelector('video'),
+  $counter = d.querySelector('.counter')
+
+function hiddeVideo() {
+  let counter = 28
+  let interval = setInterval(() => {
+    $counter.textContent = `${counter}(s)`
+    counter = counter - 1
+    if (counter == 0) {
+      $counter.classList.add('hidden')
+      $video.classList.add('hidden')
+      clearInterval(interval)
+    }
+  }, 1000)
+
+  setTimeout(() => {
+    $video.classList.toggle('video-hidden')
+  }, 28000)
+}
+
+$video.addEventListener('canplaythrough', () => {
+  $video.muted = true
+  $video.play()
+  $video.muted = false
+  hiddeVideo()
+})
